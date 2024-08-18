0e36abf63929fc2a9485dcdd0b9e5be12354f319
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2024-08-18 10:45:56 -0400

    Improve merging perf

--- lib/tailwind_merge.rb
+++ lib/tailwind_merge.rb
@@ -47,15 +47,24 @@ module TailwindMerge
       end
     end
 
-    private def merge_class_list(classes)
+    private def merge_class_list(class_list)
       # Set of class_group_ids in following format:
       # `{importantModifier}{variantModifiers}{classGroupId}`
       # @example 'float'
       # @example 'hover:focus:bg-color'
       # @example 'md:!pr'
-      class_groups_in_conflict = Set.new
+      class_groups_in_conflict = []
+      class_names = class_list.strip.split(SPLIT_CLASSES_REGEX)
 
-      classes.strip.split(SPLIT_CLASSES_REGEX).map do |original_class_name|
+      result = ""
+
+      i = class_names.length - 1
+
+      loop do
+        break if i < 0
+
+        original_class_name = class_names[i]
+
         modifiers, has_important_modifier, base_class_name, maybe_postfix_modifier_position = split_modifiers(original_class_name, separator: @config[:separator])
 
         actual_base_class_name = maybe_postfix_modifier_position ? base_class_name[0...maybe_postfix_modifier_position] : base_class_name
@@ -63,61 +72,48 @@ module TailwindMerge
 
         unless class_group_id
           unless maybe_postfix_modifier_position
-            next {
-              is_tailwind_class: false,
-              original_class_name: original_class_name,
-            }
+            # not a Tailwind class
+            result = original_class_name + (!result.empty? ? " " + result : result)
+            i -= 1
+            next
           end
 
           class_group_id = @class_utils.class_group_id(base_class_name)
 
           unless class_group_id
-            next {
-              isTailwindClass: false,
-              original_class_name: original_class_name,
-            }
-
+            # not a Tailwind class
+            result = original_class_name + (!result.empty? ? " " + result : result)
+            i -= 1
+            next
           end
 
           has_postfix_modifier = false
-
-          next {
-            is_tailwind_class: false,
-            original_class_name: original_class_name,
-          }
         end
 
         variant_modifier = sort_modifiers(modifiers).join(":")
 
         modifier_id = has_important_modifier ? "#{variant_modifier}#{IMPORTANT_MODIFIER}" : variant_modifier
-
-        {
-          is_tailwind_class: true,
-          modifier_id: modifier_id,
-          class_group_id: class_group_id,
-          original_class_name: original_class_name,
-          has_postfix_modifier: has_postfix_modifier,
-        }
-      end.reverse # Last class in conflict wins, so filter conflicting classes in reverse order.
-        .select do |parsed|
-        next(true) unless parsed[:is_tailwind_class]
-
-        modifier_id = parsed[:modifier_id]
-        class_group_id = parsed[:class_group_id]
-        has_postfix_modifier = parsed[:has_postfix_modifier]
-
         class_id = "#{modifier_id}#{class_group_id}"
 
-        next if class_groups_in_conflict.include?(class_id)
-
-        class_groups_in_conflict.add(class_id)
-
-        @class_utils.get_conflicting_class_group_ids(class_group_id, has_postfix_modifier).each do |group|
-          class_groups_in_conflict.add("#{modifier_id}#{group}")
+        # Tailwind class omitted due to pre-existing conflict
+        if class_groups_in_conflict.include?(class_id)
+          i -= 1
+          next
         end
 
-        true
-      end.reverse.map { |parsed| parsed[:original_class_name] }.join(" ")
+        class_groups_in_conflict.push(class_id)
+
+        @class_utils.get_conflicting_class_group_ids(class_group_id, has_postfix_modifier).each do |group|
+          class_groups_in_conflict.push("#{modifier_id}#{group}")
+        end
+
+        # no conflict!
+        result = original_class_name + (!result.empty? ? " " + result : result)
+
+        i -= 1
+      end
+
+      result
     end
   end
 end
